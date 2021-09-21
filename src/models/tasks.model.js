af7ec45de8c06bb8487f144cb687a91c0fc5a9f1
Giovanni Gomez R <giovanni.gomez@nuvu.cc>
2021-09-20 20:46:08 -0500

    Insertar una tareas

--- src/models/tasks.model.js
+++ src/models/tasks.model.js
@@ -1,7 +1,8 @@
 const { Schema, model } = require('mongoose');
 
 const _taskSchema = new Schema({
-    
+    task: String,
+    description: String
 });
 
 module.exports = model('Task', _taskSchema);
--- src/routes/tasks.route.js
+++ src/routes/tasks.route.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const routes = express.Router();
+const _taskSchema = require('../models/tasks.model');
 
 routes.get('/', (req, res) => {
     res.render('index');
 });
+
+routes.post('/createTask', (req, res) => {
+    const tasks = {
+        task: req.body.task,
+        description: req.body.description
+    };
+    const taskSchema = new _taskSchema(tasks);
+    taskSchema.save();
+})
 
 module.exports = routes;
